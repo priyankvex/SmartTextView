9a2b538783d7f4c02c7a0c4567cf21200430b7f0
Priyank Verma <priyankvex95@gmail.com>
2016-08-11 17:58:48 +0530

    Update the sample app.

--- app/src/main/java/com/wordpress/priyankvex/smarttextviewdemo/MainActivity.java
+++ app/src/main/java/com/wordpress/priyankvex/smarttextviewdemo/MainActivity.java
@@ -3,14 +3,12 @@ package com.wordpress.priyankvex.smarttextviewdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Toast;
 
 import com.wordpress.priyankvex.smarttextview.SmartTextCallback;
 import com.wordpress.priyankvex.smarttextview.SmartTextView;
 
 
-public class MainActivity extends AppCompatActivity implements SmartTextCallback, View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements SmartTextCallback{
 
     SmartTextView mSmartTextView;
 
@@ -25,6 +23,10 @@ public class MainActivity extends AppCompatActivity implements SmartTextCallback
 
         mSmartTextView = (SmartTextView) findViewById(R.id.textView);
         mSmartTextView.setEmailColorCode("#3cb371");
+        mSmartTextView.setPhoneNumberColorCode("#ff33aa");
+        mSmartTextView.setHashTagColorCode("#f37735");
+        mSmartTextView.setUrlColorCode("#ffc425");
+        mSmartTextView.setMentionColorCode("#57b884");
         mSmartTextView.setDetectMentions(true);
         mSmartTextView.setDetectHashTags(true);
         mSmartTextView.setText(sampleText);
@@ -33,31 +35,27 @@ public class MainActivity extends AppCompatActivity implements SmartTextCallback
 
     @Override
     public void emailClick(String email) {
-        Toast.makeText(getApplicationContext(), email, Toast.LENGTH_SHORT).show();
+        // Define your own email intent. If you don't use the SmartTextCallback, library will use the
+        // inbuilt ones.
     }
 
     @Override
     public void hashTagClick(String hashTag) {
-
+        // Do something with the hash tags
     }
 
     @Override
     public void webUrlClick(String webUrl) {
-
+        // Define what happens on url click. Library has it own default if you don't implement SmartTextCallback.
     }
 
     @Override
     public void phoneNumberClick(String phoneNumber) {
-
+        // Do something with the phone number. Library has it own default if you don't implement SmartTextCallback.
     }
 
     @Override
     public void mentionClick(String mention) {
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
+        // Do something with the mention clicks
     }
 }
